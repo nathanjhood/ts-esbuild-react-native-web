db8321dfee1cb1f9489a7159d090370227af45f5
StoneyDSP <nathanjhood@googlemail.com>
2024-08-12 21:20:14 +0200

    added 'esbuild.analyze.mjs'

---
+++ scripts/esbuild.analyze.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+
+import * as esbuild from 'esbuild';
+import fs from 'node:fs';
+import { createBuildOptions } from '../esbuild.config.mjs';
+
+const options = createBuildOptions({ minify: true, metafile: true });
+const build = await esbuild.build(options);
+const mode = process.env.npm_config_mode;
+
+if (!build.metafile) throw new Error("no build meta file was generated")
+
+if (mode === "write") {
+  fs.writeFileSync("build-meta.json", JSON.stringify(build.metafile))
+} else {
+  console.info(await esbuild.analyzeMetafile(build.metafile, {
+    verbose: false,
+  }));
+}
+
+export { }
