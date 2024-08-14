126e12207bd4eab444a22848a6d1377a40ff2f96
StoneyDSP <nathanjhood@googlemail.com>
2024-08-14 18:29:46 +0200

    don't use triple-slash directives for bundler projects
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- src/react-native-web-env.d.ts
+++ src/react-native-web-env.d.ts
@@ -1,19 +1,18 @@
-/// <reference types="esbuild" />
-/// <reference types="react-native-web" />
-/// <reference types="react-dom" />
+// /// <reference types="esbuild" />
+// /// <reference types="react-native-web" />
+// /// <reference types="react-dom" />
 
 import 'react-native-web'
+import 'react-dom'
+import 'react'
 
 // Add this to '.gitignore'
 
 
 /**
- * 
+ *
  */
-declare module '*.svg' {
-    const content: string;
-    export default content;
-}
+
 
 declare module 'react-native' {
     export {default} from 'react-native-web'
