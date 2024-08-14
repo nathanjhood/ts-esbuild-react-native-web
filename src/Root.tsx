27aafa643fccf7bf560bd1ddee4a1b68c50c7c31
StoneyDSP <nathanjhood@googlemail.com>
2024-08-14 18:30:44 +0200

    added 'Root.tsx'
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ src/Root.tsx
@@ -0,0 +1,51 @@
+// import { ScrollViewStyleReset } from 'expo-router/html';
+import React from 'react'
+
+/**
+ * Root style-reset for full-screen React Native web apps with a root `<ScrollView />` should use the following styles to ensure native parity. [Learn more](https://necolas.github.io/react-native-web/docs/setup/#root-element).
+ */
+function ScrollViewStyleReset() {
+  return (
+    <style
+      id="expo-reset"
+      dangerouslySetInnerHTML={{__html: `#root,body,html{height:100%}body{overflow:hidden}#root{display:flex}`}}
+    />
+  );
+}
+
+/**
+ * This file is web-only and used to configure the root HTML for every web page during static rendering.
+ * The contents of this function only run in Node.js environments and do not have access to the DOM or browser APIs.
+ */
+export default function Root({ children }: React.PropsWithChildren) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+        {/*
+          Disable body scrolling on web. This makes ScrollView components work closer to how they do on native.
+          However, body scrolling is often nice to have for mobile web. If you want to enable it, remove this line.
+        */}
+        <ScrollViewStyleReset />
+
+        {/* Using raw CSS styles as an escape-hatch to ensure the background color never flickers in dark-mode. */}
+        <style dangerouslySetInnerHTML={{ __html: responsiveBackground }} />
+        {/* Add any additional <head> elements that you want globally available on web... */}
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
+
+const responsiveBackground = `
+body {
+  background-color: #fff;
+}
+@media (prefers-color-scheme: dark) {
+  body {
+    background-color: #000;
+  }
+}`;
