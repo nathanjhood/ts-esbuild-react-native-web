5a3390c61c2e308a83ac348c7e312ceb43cc98b8
StoneyDSP <nathanjhood@googlemail.com>
2024-08-28 20:38:18 +0200

    removed unused styles
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,17 @@
 import * as React from 'react';
-import { AppRegistry, StyleSheet, type ViewStyle, type TextStyle } from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
 import './index.css';
 
 const Index: () => JSX.Element = () => {
   return (
     <React.StrictMode>
-      <App body={styles.body} p={styles.p} code={styles.code} />
+      <App />
     </React.StrictMode>
   )
 }
 
-AppRegistry.registerComponent('App', () => App);
-AppRegistry.runApplication('App', {
+AppRegistry.registerComponent('Index', () => Index);
+AppRegistry.runApplication('Index', {
   rootTag: document.getElementById('root')
 });
-
-const styles = StyleSheet.create<{
-  body: ViewStyle,
-  code: TextStyle,
-  p: TextStyle
-}>({
-  body: {
-    margin: 0
-  },
-  code: {
-    fontFamily: "monospace"
-  },
-  p: {
-    color: "white"
-  }
-})
