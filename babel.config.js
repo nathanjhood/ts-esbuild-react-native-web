175554d5f2a6d1ed6e44b7ea0788fdaa805f25c5
StoneyDSP <nathanjhood@googlemail.com>
2024-08-29 13:04:06 +0200

    added react-native configs for babel, ts, jest
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: ["module:@react-native/babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          "^react-native$": "react-native-web",
+        },
+      },
+    ],
+  ],
+};
