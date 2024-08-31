78a50a8836354bb90e8f378bfcd3bf6914386690
StoneyDSP <nathanjhood@googlemail.com>
2024-08-31 17:29:21 +0200

    keep Jest import in test file
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-// import { test, expect, describe } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
 import { render, screen, cleanup } from '@testing-library/react';
 import App from './App';
 
