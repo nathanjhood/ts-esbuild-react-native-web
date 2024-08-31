4672fa97a61b489242c06dbe15456bf548937dbf
StoneyDSP <nathanjhood@googlemail.com>
2024-08-31 17:20:17 +0200

    added setupProxy for backend integration
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

---
+++ src/setupProxy.js
@@ -0,0 +1,13 @@
+/** @see https://github.com/docker/awesome-compose/blob/master/react-java-mysql/frontend/src/setupProxy.js */
+
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "http://backend:8080",
+      pathRewrite: { "^/api": "" },
+    })
+  );
+};
