d38a88cff4ff01879ef120c6c6ad8e5d8f3c7f85
StoneyDSP <nathanjhood@googlemail.com>
2024-10-09 22:30:36 +0200

    edit 'prettier.config.mjs' - use latest
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,17 +1,38 @@
-//@ts-check
+/**
+ * @file prettier.config.mjs
+ * @author Nathan J. Hood <nathanjhood@googlemail.com>
+ * @brief
+ * @version 0.1.0
+ * @date 2024-09-05
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
 
 /**
  * @see https://prettier.io/docs/en/configuration.html
  * @type {import("prettier").Config}
  */
 const config = {
-    trailingComma: 'es5',
-    tabWidth: 2,
-    semi: true,
-    singleQuote: false,
-    useTabs: false,
-    printWidth: 80,
-    endOfLine: 'lf',
-}
+  /**
+   * '.editorconfig' sets the below, don't set these here:
+   */
+  // tabWidth: 2,
+  // useTabs: false,
+  // printWidth: 80,
+  // endOfLine: "lf",
+  /**
+   * custom
+   */
+  semi: true,
+  parser: "babel", // inferred from environment
+  plugins: ["prettier-plugin-tailwindcss"],
+  // from Expo:
+  arrowParens: "avoid",
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: "all",
+};
 
-export default config
+export default config;
