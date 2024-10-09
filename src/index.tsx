e3f8fef597d240a90d09b28f4a26032b4d60aed8
StoneyDSP <nathanjhood@googlemail.com>
2024-10-09 22:29:00 +0200

    edit 'src/index.tsx' - added service worker registration
    Signed-off-by: StoneyDSP <nathanjhood@googlemail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,26 @@
-import * as React from 'react';
-import { AppRegistry } from 'react-native';
-import App from './App';
-import './index.css';
+import React = require("react");
+import ReactDOM = require("react-dom/client");
+import reportWebVitals = require("./reportWebVitals");
+import serviceWorkerRegistration = require("./serviceWorkerRegistration");
 
-const Index: () => JSX.Element = () => {
-  return (
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
-}
+import App = require("./App");
 
-AppRegistry.registerComponent('Index', () => Index);
-AppRegistry.runApplication('Index', {
-  rootTag: document.getElementById('root')
-});
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers:
+// https://create-react-app.dev/docs/making-a-progressive-web-app
+serviceWorkerRegistration.register();
